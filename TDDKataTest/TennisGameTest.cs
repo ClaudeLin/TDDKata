a543be0a1b44a51c47f83edc965b7ac8fc36a295
CladueLin <linc27595@gmail.com>
2021-04-28 01:36:41 +0800

    Add first test with red light

---
+++ TDDKataTest/TennisGameTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using TDDKata;
+
+namespace TDDKataTest
+{
+    public class TennisGameTest
+    {
+        [Test]
+        public void Love_All()
+        {
+            var tennisGame = new TennisGame();
+            var actual = tennisGame.Score();
+            Assert.AreEqual("Love All",actual);
+        }
+        
+    }
+}
