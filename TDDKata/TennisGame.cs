6e36ca4dea6804381e948d15226cfdc0644406bc
CladueLin <linc27595@gmail.com>
2021-04-28 23:43:13 +0800

    Pass second test

--- TDDKata/TennisGame.cs
+++ TDDKata/TennisGame.cs
@@ -2,13 +2,25 @@
 {
     public class TennisGame
     {
+        private TennisPlayer _player1;
+
+        public TennisGame()
+        {
+            _player1 = new TennisPlayer();
+        }
+
         public string Score()
         {
+            if (_player1.Score == 1)
+            {
+                return "Fifteen Love";
+            }
             return "Love All";
         }
 
         public void FirstPlayerGetScore()
         {
+            _player1.GetScore();
         }
     }
 }
---
+++ TDDKata/TennisPlayer.cs
@@ -0,0 +1,12 @@
+﻿namespace TDDKata
+{
+    public class TennisPlayer
+    {
+        public int Score { get; set; }
+
+        public void GetScore()
+        {
+            Score++;
+        }
+    }
+}
