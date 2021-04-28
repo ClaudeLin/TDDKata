f131fbd4208f698bacf878c44782746dad033c38
CladueLin <linc27595@gmail.com>
2021-04-29 00:01:49 +0800

    Refactoring extract dictionary to mapping score wording

--- TDDKata/TennisGame.cs
+++ TDDKata/TennisGame.cs
@@ -1,29 +1,40 @@
-﻿namespace TDDKata
+﻿using System.Collections.Generic;
+
+namespace TDDKata
 {
     public class TennisGame
     {
         private readonly TennisPlayer _player1;
         private readonly TennisPlayer _player2;
+        private Dictionary<int,string> scoreMapping ;
 
         public TennisGame()
         {
+            scoreMapping = new Dictionary<int, string>()
+            {
+                {0,"Love"},
+                {1,"Fifteen"},
+                {2,"Thirty"},
+            };
             _player2 = new TennisPlayer();
             _player1 = new TennisPlayer();
         }
 
         public string Score()
         {
-            if (_player1.Score == 2)
+            
+            if (_player1.Score == 2 && _player2.Score!=_player1.Score)
             {
-                return "Thirty Love";
+                return scoreMapping[_player1.Score] + " " + scoreMapping[_player2.Score];
             }
-            if (_player1.Score == 1 && _player2.Score == 1)
+            if (_player1.Score == 1 && _player2.Score!=_player1.Score)
             {
-                return "Fifteen All";
+                return scoreMapping[_player1.Score] + " " + scoreMapping[_player2.Score];
             }
-            if (_player1.Score == 1)
+
+            if (_player1.Score == _player2.Score)
             {
-                return "Fifteen Love";
+                return scoreMapping[_player1.Score] + " All";
             }
             
             return "Love All";
