e838599fa8c19392d276a1dd0b434f2032659b5d
ClaudeLin <linc27595@gmail.com>
2021-05-01 01:55:32 +0800

    Pass 3rd test

--- TDDKata/TennisGame.cs
+++ TDDKata/TennisGame.cs
@@ -3,14 +3,20 @@
     public class TennisGame
     {
         private readonly Player _player1;
+        private readonly Player _receiver;
 
         public TennisGame()
         {
+            _receiver = new Player();
             _player1 = new Player();
         }
 
         public string GetScore()
         {
+            if (_player1.Point == 1 && _receiver.Point == 1)
+            {
+                return "Fifteen All";
+            }
             if (_player1.Point == 1)
             {
                 return "Fifteen Love";
@@ -26,6 +32,7 @@
 
         public void ReceiverGetScore()
         {
+            _receiver.Point++;
         }
     }
 }
