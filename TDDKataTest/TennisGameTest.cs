48d97dbb36641d4f510b7810c1eb5dec385ea904
ClaudeLin <linc27595@gmail.com>
2021-05-03 23:53:52 +0800

    Add 1st test love all get red light

---
+++ TDDKataTest/TennisGameTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using TDDKata;
+
+namespace TDDKataTest
+{
+    public class TennisGameTest
+    {
+        [Test]
+        public void Love_All()
+        {
+            var expected = "Love All";
+            var tennisGame = new TennisGame();
+            var actual=tennisGame.CurrentScore();
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
