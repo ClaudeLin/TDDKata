74fd12a338f386474106b8fe03627a3c3f0121df
CladueLin <linc27595@gmail.com>
2021-05-05 00:32:16 +0800

    Add 1st with green light and refactoring

---
+++ TDDKataTest/TennisGameTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TDDKata;
+
+namespace TDDKataTest
+{
+    public class TennisGameTest
+    {
+        private TennisGame _tennisGame;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tennisGame = new TennisGame();
+        }
+
+        [Test]
+        public void Love_All()
+        {
+            CurrentScoreShouldBe("Love All");
+        }
+
+        private void CurrentScoreShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, _tennisGame.CurrentScore());
+        }
+    }
+}
