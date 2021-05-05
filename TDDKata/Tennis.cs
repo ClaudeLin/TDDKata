e5f283eddfbcbaca703ecdbab43d13c348ef5e68
ClaudeLin <linc27595@gmail.com>
2021-05-05 23:42:12 +0800

    Add 1 test with green light and refactoring

---
+++ TDDKata/Tennis.cs
@@ -0,0 +1,10 @@
+﻿namespace TDDKata
+{
+    public class Tennis
+    {
+        public string GetCurrentScore()
+        {
+            return "Love All";
+        }
+    }
+}
---
+++ TDDKataTest/TennisTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using TDDKata;
+
+namespace TDDKataTest
+{
+    public class TennisTest
+    {
+        private Tennis _tennis;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tennis = new Tennis();
+        }
+        [Test]
+        public void Love_All()
+        {
+            CurrentScoreShouldBe("Love All");
+        }
+
+        private void CurrentScoreShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, _tennis.GetCurrentScore());
+        }
+    }
+}
